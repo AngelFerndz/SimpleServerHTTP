5fb6be745b640c3e2a6bf275345e651ea2de4294
Fethore <21150449+AngelFerndz@users.noreply.github.com>
2021-01-14 14:30:42 -0500

    SaveCommand implemented to save data from Clipboard.

--- src/dev/angelf/simpleserverhttp/commands/SaveCommand.java
+++ src/dev/angelf/simpleserverhttp/commands/SaveCommand.java
@@ -1,9 +1,23 @@
 package dev.angelf.simpleserverhttp.commands;
 
+import dev.angelf.simpleserverhttp.filesystem.WriteFile;
+import dev.angelf.simpleserverhttp.tools.Clipboard;
+import dev.angelf.simpleserverhttp.tools.Logger;
+
 public class SaveCommand implements Command {
 
     @Override
     public void run(String command) {
+        String[] elements = command.split(" ");
+        if (elements.length > 1) {
+            save(elements[1]);
+        } else {
+            Logger.log("Specify what file to read.");
+        }
+    }
 
+    private void save(String FileName){
+        String filePath = "file/" + FileName;
+        WriteFile.write(Clipboard.get_data(), filePath);
     }
 }
